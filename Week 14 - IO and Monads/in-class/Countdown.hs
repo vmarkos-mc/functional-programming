2f34c677439889e2cfeaacd04081455e8944aaed
vmarkos-mc <v.markos@mc-class.gr>
2026-02-10 15:46:02 +0200

    Add W14 in-class

--- Week 14 - IO and Monads/in-class/Countdown.hs
+++ Week 14 - IO and Monads/in-class/Countdown.hs
@@ -9,16 +9,21 @@ data Operator = Plus | Minus | Times | Div
 -- captured by the `Value` constructor either an "inner" node of the expression
 -- tree, captured by `Node`.
 data Expression = Value Int
-                | Node Expression Operator Expression
+                | Node ExpressionInstance Operator ExpressionInstance
+    deriving Show
+
+-- types in Haskell are just abbrieviations or collections of similar objects, more or less like C's structures.
+-- ADTs (data types) are more complex constructs that also allow for recursive definitions.
+newtype ExpressionInstance = Instance (Expression, Int)
     deriving Show
 
 -- Utilities
 -- Checks whether an operator can be applied on two arguments
 valid :: Operator -> Int -> Int -> Bool
 valid Minus x y = x - y > 0
-valid Div x y   = mod x y == 0
+valid Div x y   = mod x y == 0 && y /= 1
 valid Plus x y  = x >= y
-valid Times x y = x >= y
+valid Times x y = x >= y && x /= 1 && y /= 1
 -- valid _ _ _     = True -- We do not match Plus / Times explicitly.
 
 -- Applies an operator on two arguments
@@ -52,29 +57,28 @@ sublist (x:xs)  = ys ++ map (x:) ys
     where ys = sublist xs
 
 -- Generates all valid expressions
-exprs :: [Int] -> [Expression]
+exprs :: [Int] -> [ExpressionInstance]
 exprs []    = []
-exprs [x]   = [Value x]
+exprs [x]   = [Instance (Value x, x)]
 exprs ls@(x:xs) = [s | l <- subbags ls, s <- combine l]
 
-combine :: [Int] -> [Expression]
+combine :: [Int] -> [ExpressionInstance]
 combine []  = []
-combine [x] = [Value x]
+combine [x] = [Instance (Value x, x)]
 combine xs  = [ e | (l, r) <- nesplit xs, le <- combine l, re <- combine r, e <- combine' le re]
 
 -- More compact way to write combine':
 --  * Create a list of all operators, and
 --  * "loop" over them and filter out invalid operations.
-combine' :: Expression -> Expression -> [Expression]
-combine' a b = [Node a op b | op <- ops, valid op x y ]
+combine' :: ExpressionInstance -> ExpressionInstance -> [ExpressionInstance]
+combine' x@(Instance (a, v)) y@(Instance (b, u)) = [Instance (Node x op y, apply op v u) | op <- ops, valid op v u ]
     where
         ops = [Plus, Minus, Times, Div]
-        x = eval a
-        y = eval b
 
-eval :: Expression -> Int
-eval (Value n)      = n
-eval (Node l op r)  = apply op (eval l) (eval r)
+-- eval is useless, given that we compute all expression values at the time they are created
+-- eval :: Expression -> Int
+-- eval (Value n)      = n
+-- eval (Node l op r)  = apply op (eval l) (eval r)
 
 solve :: [Int] -> Int -> [Expression]
-solve ns t  = [e | e <- exprs ns, eval e == t]
+solve ns t  = [e | Instance (e, v) <- exprs ns, v == t]
--- Week 14 - IO and Monads/in-class/notes.md
+++ Week 14 - IO and Monads/in-class/notes.md
@@ -7,4 +7,6 @@ For the case `[25, 1, 7, 4, 2, 8], 910`
 | Attempt Decsription           | Time (s)      | Memory (bytes)            |
 |-------------------------------|---------------|---------------------------|
 | Brute force                   | 30.83         | 13,717,830,624            |
-| Commutativity                 |  5.12         |  2,555,268,976            |
+| +Commutativity                |  5.12         |  2,555,268,976            |
+| +Simultaneous evaluation      |  2.43         |  1,626,647,040            |
+| +Trivial operations           |  1.70         |  1,106,345,296            |
